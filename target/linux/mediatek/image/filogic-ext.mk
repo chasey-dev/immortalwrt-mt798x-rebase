f89d12e782b81011c04c6abf53ac32ee54d01b3c
wn2try <108980673+wn2try@users.noreply.github.com>
2026-06-01 14:48:31 +0800

    mediatek: filogic: add support for Ruijie RG-X60 New
    Device specs:
    - SoC: MediaTek MT7986A
    - Switch: MediaTek MT7531AE + Airoha EN8811HN
    - WiFi: Mediatek MT7975N + MT7975PN
    - Flash: 128MB SPI-NAND
    - RAM: 512MB DDR3
    - Ethernet ports: 4
    - Leds: 4
    - Buttons: 2
    
    mtd ubi size: 109568k = 107m

--- target/linux/mediatek/image/filogic-ext.mk
+++ target/linux/mediatek/image/filogic-ext.mk
@@ -75,6 +75,17 @@ define Device/ruijie_rg-x30e-pro
 endef
 TARGET_DEVICES += ruijie_rg-x30e-pro
 
+define Device/ruijie_rg-x60-new
+  DEVICE_VENDOR := Ruijie
+  DEVICE_MODEL := RG-X60 New
+  DEVICE_DTS := mt7986a-ruijie-rg-x60-new
+  DEVICE_DTS_DIR := ../dts-ext
+  DEVICE_PACKAGES := kmod-mt7915e kmod-mt7986-firmware \
+  mt7986-wo-firmware kmod-phy-airoha-en8811h kmod-mtd-rw
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += ruijie_rg-x60-new
+
 define Device/sl_3000-emmc
   DEVICE_VENDOR := SL
   DEVICE_MODEL := 3000 eMMC

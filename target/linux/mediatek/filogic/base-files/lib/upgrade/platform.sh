cf954a1ccb2a3d568e8feb03223143405022bd92
JimLee1996 <59061463@qq.com>
2026-06-30 08:49:54 +0800

    feat: add mediatek,zhao-7981r128
    Signed-off-by: JimLee1996 <59061463@qq.com>

--- target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
+++ target/linux/mediatek/filogic/base-files/lib/upgrade/platform.sh
@@ -203,6 +203,7 @@ platform_do_upgrade() {
 	huasifei,wh3000-pro-nand|\
 	huasifei,wh3000r-nand|\
 	jiorouter,ax6000-jidu6101|\
+	mediatek,zhao-7981r128|\
 	ruijie,rg-x30e-pro)
 		CI_UBIPART="ubi"
 		nand_do_upgrade "$1"
@@ -377,6 +378,7 @@ platform_check_image() {
 		;;
 	creatlentem,clt-r30b1|\
 	creatlentem,clt-r30b1-112m|\
+	mediatek,zhao-7981r128|\
 	nradio,c8-668gl)
 		# tar magic `ustar`
 		magic="$(dd if="$1" bs=1 skip=257 count=5 2>/dev/null)"
